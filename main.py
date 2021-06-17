8d76d9ea06bf3d51aa7bf69dfba8907ccee40ebf
Pickledtezcat <johnmtopple@gmail.com>
2021-06-17 01:29:16 +0000

    continuing project

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ medical_data_visualizer.draw_cat_plot()
 medical_data_visualizer.draw_heat_map()
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+# disable unit tests during work
+#main(module='test_module', exit=False)
--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,21 +3,38 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-#starting project
+#continuing project
 
 # Import data
-df = None
+df = pd.read_csv(
+'medical_examination.csv')
+
+weight_series = df["weight"]
+height_series = df["height"]
+hm = height_series * 0.01
+
+bmi = weight_series / (hm * hm)
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] = bmi > 25
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 
+cholesterol_series = df["cholesterol"]
+gluc_series = df["gluc"]
+
+cholesterol_series = cholesterol_series
+
+df.loc[df["cholesterol"] == 1, "cholesterol"] = 0
+df.loc[df["cholesterol"] > 1, "cholesterol"] = 1
+
+df.loc[df["gluc"] == 1, "gluc"] = 0
+df.loc[df["gluc"] > 1, "gluc"] = 1
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = df.melt(['cholesterol', 'gluc', 'smoke', 'alco', 'active','overweight'])
 
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
@@ -28,8 +45,8 @@ def draw_cat_plot():
 
 
     # Do not modify the next two lines
-    fig.savefig('catplot.png')
-    return fig
+    #fig.savefig('catplot.png')
+    #return fig
 
 
 # Draw Heat Map
@@ -46,12 +63,12 @@ def draw_heat_map():
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    #fig, ax = None
 
     # Draw the heatmap with 'sns.heatmap()'
 
 
 
     # Do not modify the next two lines
-    fig.savefig('heatmap.png')
-    return fig
+    #fig.savefig('heatmap.png')
+    #return fig
