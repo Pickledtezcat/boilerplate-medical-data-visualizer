42a7a8b875b7f01b8eeb517c094154bf1f74ca6f
Pickledtezcat <johnmtopple@gmail.com>
2021-06-17 02:01:57 +0000

    Added cat plot replit running slow, rstarting

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -34,19 +34,17 @@ df.loc[df["gluc"] > 1, "gluc"] = 1
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = df.melt(['cholesterol', 'gluc', 'smoke', 'alco', 'active','overweight'])
-
+    df_cat = pd.melt(df, id_vars= ["cardio"], value_vars= ['cholesterol', 'gluc', 'smoke', 'alco', 'active','overweight'])
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    #df_cat = None
 
     # Draw the catplot with 'sns.catplot()'
-
-
-
+    g = sns.catplot(kind="count", x="variable", hue="value", col="cardio", data=df_cat) 
+        
     # Do not modify the next two lines
-    #fig.savefig('catplot.png')
-    #return fig
+    fig.savefig('catplot.png')
+    return fig
 
 
 # Draw Heat Map
